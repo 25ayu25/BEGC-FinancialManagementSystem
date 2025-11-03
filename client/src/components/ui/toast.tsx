addf4747a8519f5858d350a2d0a7944e37ba255f
25ayu25 <ayuudlls@gmail.com>
2025-11-03 13:34:38 -0600

    Update toast.tsx

--- client/src/components/ui/toast.tsx
+++ client/src/components/ui/toast.tsx
@@ -14,7 +14,8 @@ const ToastViewport = React.forwardRef<
   <ToastPrimitives.Viewport
     ref={ref}
     className={cn(
-      "fixed top-0 z-[1100] flex max-h-screen w-full flex-col-reverse p-4 sm:bottom-0 sm:right-0 sm:top-auto sm:flex-col md:max-w-[420px]",
+      // No fixed/bottom/right here – position is controlled by the caller (Toaster)
+      "z-[1100] flex max-h-screen w-full flex-col gap-2 p-4 md:max-w-[420px]",
       className
     )}
     {...props}
